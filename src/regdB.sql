2f9f5519582c31392ecde070627e016cff3101ac
Sujata Dev <sujatadev97@gmail.com>
2017-04-16 12:42:14 +0530

    reg database added

---
+++ src/regdB.sql
@@ -0,0 +1,28 @@
+_HiStOrY_V2_
+use reg
+create table registration
+(
+Username varchar(255),
+email varchar(255),
+primary key(email)
+);
+insert into registration values('abc','abc1','def','def')
+;
+insert into registration values(abc,abc1,def,def)
+;
+drop table registration
+;
+create table registration
+(
+username varchar(255),
+email varchar(255),
+primary key(email)
+);
+insert into registration values('abc','abc1','def','def');
+show tables
+;
+select * from registration
+;
+select * from registration;
+delete from registration where email='';
+select * from registration;
